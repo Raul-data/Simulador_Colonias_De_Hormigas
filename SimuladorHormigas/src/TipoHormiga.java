7779843f8f8eec0528bb8c06ab2a058c64ddc426
Raul-data <rauljusto7@outlook.com>
2025-10-02 14:33:18 +0200

    enum

--- SimuladorHormigas/src/TipoHormiga.java
+++ SimuladorHormigas/src/TipoHormiga.java
@@ -1,2 +1,9 @@
-public class TipoHormiga {
+//Enumeracion que define los tipos de hormigas
+public enum TipoHormiga {
+
+    //Atributos
+    private final String simbolo; //Simbolo que tendra la hormiga
+    private final String nombre; //Nombre que dira el tipo de hormiga
+
+
 }
