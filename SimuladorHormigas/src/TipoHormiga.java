8147f7589069634f9dd142c683fcc4519df085a0
Raul-data <rauljusto7@outlook.com>
2025-10-03 16:06:26 +0200

    Añadir enum TipoHormiga con tipos OBRERA, GUERRERA y REINA

--- SimuladorHormigas/src/TipoHormiga.java
+++ SimuladorHormigas/src/TipoHormiga.java
@@ -1,9 +1,26 @@
 //Enumeracion que define los tipos de hormigas
 public enum TipoHormiga {
+    //Ceamos las constantes que seran los tipos de hormiga
+    OBRERA("O", "Obrera"),
+    GUERRERA("G","Guerra"),
+    REINA("R", "Reina");
 
     //Atributos
     private final String simbolo; //Simbolo que tendra la hormiga
     private final String nombre; //Nombre que dira el tipo de hormiga
+
+    TipoHormiga(String simbolo, String nombre) {
+        this.simbolo = simbolo;
+        this.nombre = nombre;
+    }
+
+    public String getSimbolo() {
+        return simbolo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 
 
 }
