832bdb540ffe06406fce72083328b79ec8513a7a
Raul-data <rauljusto7@outlook.com>
2025-10-06 12:24:55 +0200

    creamos la clase HormigaObrera con su constructor y dejamos vacion el metodo run

--- SimuladorHormigas/src/HormigaObrera.java
+++ SimuladorHormigas/src/HormigaObrera.java
@@ -1,2 +1,14 @@
-public class HormigaObrera {
+public class HormigaObrera extends Hormiga {
+    //Creamos el constructor,que recibe el id y la poscion donde nace la obreara
+    //0(1)
+    public HormigaObrera(String id,Posicion posicionInicial){
+        super(id, TipoHormiga.OBRERA, posicionInicial); // de esta manera hacemos la llamada al constructor padre
+    }
+
+    //Metodo run vacio
+    @Override
+    public void run(){
+        //por ahora no le metemos nada son hilos
+    }
+
 }
